c286acd2bb895f11b875cd92343a90fe9960ccd4
Dönges Saska I <saska@hpf-20.cs.helsinki.fi>
2017-12-19 18:03:29 +0200

    fuu

--- config/database.php
+++ config/database.php
@@ -10,7 +10,6 @@
 
     // Huomioi että MySQL:n käyttäjätunnus ja salasana ovat tietoa joka olisi syytä pitää
     // salassa, eli älä talleta tätä Githubiin.
-    private static $con_settings = parse_url(getenv('DATABASE_URL'))
 
     private static $connection_config = array(
       'psql' => array(
