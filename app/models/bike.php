f74affa8f0df685d56f51e69eb31de533c1deba9
Dönges Saska I <saska@hpf-20.cs.helsinki.fi>
2017-12-27 20:09:04 +0200

    goddamn syntax. Why does a dynamically typed language require semicolonsgit pushgit push!

--- app/models/bike.php
+++ app/models/bike.php
@@ -4,6 +4,28 @@ class Bike extends BaseMoodel {
 	public $id, $user_id, $distance, $name, $model, $link, $year, $in_use, $retired, $description;
 
 	public function __construct($attributes) {
-		parent::__construct($attributes)
+		parent::__construct($attributes);
+	}
+
+	public static function all() {
+		$query = DB::connection()->prepare('SELECT * FROM bike');
+		$query->execute();
+		$rows = $query->fetchAll();
+    	$games = array();
+    	foreach($rows as $row){
+      		$games[] = new Game(array(
+        		'id' => $row['id'],
+        		'user_id' => $row['user_id'],
+        		'distance' => $row['distance'],
+        		'name' => $row['name'],
+        		'model' => $row['model'],
+        		'link' => $row['link'],
+        		'year' => $row['year'],
+        		'in_use' => $row['in_use'],
+        		'retired' => $row['retired'],
+        		'description' => $row['description']
+      		));
+    	}
+	    return $games;
 	}
 }
