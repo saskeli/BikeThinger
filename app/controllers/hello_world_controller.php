f0ff0722d5a5bd2423bb65854ef847ceb452c4fd
Dönges Saska I <saska@hpf-20.cs.helsinki.fi>
2017-12-27 15:39:20 +0200

    plans

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -1,14 +1,33 @@
 <?php
 
-  class HelloWorldController extends BaseController{
-
-    public static function index(){
-      // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
-    }
-
-    public static function sandbox(){
-      // Testaa koodiasi täällä
-      View::make('helloworld.html');
-    }
+class HelloWorldController extends BaseController{
+  public static function index(){
+    // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
+ 	  View::make('home.html');
   }
+  public static function sandbox(){
+    // Testaa koodiasi täällä
+    View::make('helloworld.html');
+  }
+  public static function Bikes(){
+    View::make('plans/bikes.html');
+  }
+  public static function Gear(){
+    View::make('plans/gear.html');
+  }
+  public static function Components(){
+    View::make('plans/components.html');
+  }
+  public static function Details(){
+    View::make('plans/details.html');
+  }
+  public static function Edit(){
+    View::make('plans/edit.html');
+  }
+  public static function Login(){
+    View::make('plans/login.html');
+  }
+  public static function Spessu(){
+    View::make('plans/details.html');
+  }
+}
--- config/routes.php
+++ config/routes.php
@@ -1,9 +1,25 @@
 <?php
 
-  $routes->get('/', function() {
-    HelloWorldController::index();
-  });
+$routes->get('/', function() {
+  HelloWorldController::index();
+});
 
-  $routes->get('/sandbox', function() {
-    HelloWorldController::sandbox();
-  });
+$routes->get('/sandbox', function() {
+  HelloWorldController::sandbox();
+});
+
+$routes->get('/bikes', function() {
+  HelloWorldController::Bikes();
+});
+
+$routes->get('/gear', function() {
+  HelloWorldController::Gear();
+});
+
+$routes->get('/components', function() {
+  HelloWorldController::Components();
+});
+
+$routes-> get('/login', function() {
+  HelloWorldController::Login();
+});
