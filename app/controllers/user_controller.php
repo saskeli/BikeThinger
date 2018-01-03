dc82d7ddbee396e23ccb8d35dabc6244fc4751db
Dönges Saska I <saska@hpf-20.cs.helsinki.fi>
2018-01-03 17:32:27 +0200

    hopefully fixed login.. again

--- app/controllers/user_controller.php
+++ app/controllers/user_controller.php
@@ -6,7 +6,7 @@ class UserController extends BaseController {
   }
 
   public static function authenticate() {
-    $user = User::authenticate($_POST['username'], $_Post['password']);
+    $user = User::authenticate($_POST['username'], $_POST['password']);
     if (!$user) {
       View::make('login.html', array('error' => 'Invalid username/password'));
     } else {
